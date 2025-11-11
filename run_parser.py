48849ebd701dce76b19572892fb0c735aa9ef0a9
jatoapan <jatoapan@espol.edu.ec>
2025-11-10 20:37:41 -0500

    [ADD] Added parser tests and fixed some conflicts

--- run_parser.py
+++ run_parser.py
@@ -4,7 +4,6 @@ from go_analyzer.core.parser import run_parser
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python run_parser.py tests/<file.go> <username>")
-        print("Example: python run_parser.py tests/jfernandez_parser.go 'Juan Francisco Fernandez'")
         sys.exit(1)
     else:
         file_path = sys.argv[1]
